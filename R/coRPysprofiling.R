f98aa4d4f454ec21185c0e1b95e50aa083b09a18
Ivy Zhang <ivyzhang1017@hotmail.com>
2021-03-03 22:10:16 -0700

    Add space saver for four R functions

---
+++ R/coRPysprofiling.R
@@ -0,0 +1,15 @@
+corpus_analysis <- function(corpus) {
+
+}
+
+corpus_viz <- function(corpus, stats) {
+
+}
+
+corpora_compare <- function(corpus1, corpus2, metric="cosine_similarity") {
+
+}
+
+corpora_best_match <- function(refDoc, corpora, metric="cosine_similarity") {
+
+}
